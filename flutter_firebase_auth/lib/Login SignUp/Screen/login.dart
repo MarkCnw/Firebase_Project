4e3ea3c37d94123ace54c662b7a4e6a429591249
MarkCnw <chinnawong554@gmail.com>
2025-03-16 18:21:03 +0700

    Add Google Sign-In functionality and update dependencies

--- flutter_firebase_auth/lib/Login SignUp/Screen/login.dart
+++ flutter_firebase_auth/lib/Login SignUp/Screen/login.dart
@@ -27,27 +27,29 @@ class _SignupScreenState extends State<LoginScreen> {
     passwordController.dispose();
   }
 
-   void loginUser() async {
+  void loginUser() async {
     String res = await AuthServicews().loginUser(
-        email: emailController.text,
-        password: passwordController.text,
+      email: emailController.text,
+      password: passwordController.text,
     );
 
     if (res == "success") {
       setState(() {
         isLoading = true;
       });
-      Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (context) => HomeScreen(),
-      ),
-    );
-    }else{
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        ),
+      );
+    } else {
       setState(() {
         isLoading = false;
       });
       showSnackbar(context, res);
     }
   }
+
   @override
   Widget build(BuildContext context) {
     double height = MediaQuery.of(context).size.height;
@@ -68,35 +70,65 @@ class _SignupScreenState extends State<LoginScreen> {
                 hintText: "Enter your email",
                 icon: Icons.email),
             TextFieldInput(
-                isPass: true,
-                textEditingController: passwordController,
-                hintText: "Enter your password",
-                icon: Icons.lock,
-                ),
+              isPass: true,
+              textEditingController: passwordController,
+              hintText: "Enter your password",
+              icon: Icons.lock,
+            ),
             MyButton(onTab: loginUser, text: "Log In"),
             ForgotPassword(),
             SizedBox(height: height / 15),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "Dont have an account?",
-                  style: TextStyle(fontSize: 16),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 25,vertical: 10),
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey,
                 ),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => SignUpScreen()));
-                  },
-                  child: Text(
-                    "Sign Up",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold, fontSize: 16),
+                  onPressed: () {},
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8),
+                        child: Image.network(
+                          "https://logos-world.net/wp-content/uploads/2020/09/Google-Symbol.png",
+                          height: 35,
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "continue with Google",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      )
+                    ],
+                  )),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 10, left: 100),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Dont have an account?",
+                    style: TextStyle(fontSize: 16),
                   ),
-                )
-              ],
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignUpScreen()));
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
             )
           ],
         ),
