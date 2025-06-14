aa677dc6057f8f92ba776ddf1e1f425aa28e9ed9
MarkCnw <chinnawong554@gmail.com>
2025-06-14 23:47:10 +0700

    feat: Integrate Firebase into the project with necessary configurations and dependencies

--- planmate/lib/main.dart
+++ planmate/lib/main.dart
@@ -1,7 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:planmate/Onboarding/Presentation/onboarding_screen.dart';
+import 'package:planmate/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
